1708311712157db22dab96cfa0e4e87e0854c5b0
yanfeifengwu <15139097394@163.com>
2016-04-05 13:30:53 +0800

    first

---
+++ HuiYan/HuiYan/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  huiyan
+//
+//  Created by 华印mac－002 on 16/4/5.
+//  Copyright © 2016年 com.huayin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HuiYan/HuiYanTests/HuiYanTests.m
@@ -0,0 +1,39 @@
+//
+//  huiyanTests.m
+//  huiyanTests
+//
+//  Created by 华印mac－002 on 16/4/5.
+//  Copyright © 2016年 com.huayin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface huiyanTests : XCTestCase
+
+@end
+
+@implementation huiyanTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
