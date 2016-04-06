270ffa10c22821d41e3ff6fce17bc2dfe1b7684a
maxLoveCode <maxkidy@gmail.com>
2016-04-06 10:34:52 +0800

    add Unit test

---
+++ HuiYan/HuiYan/Helpers/UnitTest.m
@@ -0,0 +1,13 @@
+//
+//  UnitTest.m
+//  huiyan
+//
+//  Created by 华印mac-001 on 16/4/6.
+//  Copyright © 2016年 com.huayin. All rights reserved.
+//
+
+#import "UnitTest.h"
+
+@implementation UnitTest
+
+@end
--- huiyan/huiyan/Helpers/ServerManager.m
+++ huiyan/huiyan/Helpers/ServerManager.m
@@ -9,12 +9,6 @@
 #import "ServerManager.h"
 
 @implementation ServerManager
-+ (ServerManager *)instance{
-    static ServerManager *Instance = nil;
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        Instance = [[ServerManager alloc]init];
-    });
-    return Instance;
-}
+
+
 @end
