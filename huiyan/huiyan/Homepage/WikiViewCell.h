4137ff9f16887bba2ca871d76a7742c4853577d3
yanfeifengwu <15139097394@163.com>
2016-04-06 14:27:18 +0800

    pull

---
+++ huiyan/huiyan/Homepage/WikiViewCell.h
@@ -0,0 +1,13 @@
+//
+//  WikiViewCell.h
+//  huiyan
+//
+//  Created by 华印mac－002 on 16/4/6.
+//  Copyright © 2016年 com.huayin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WikiViewCell : UITableViewCell
+
+@end
