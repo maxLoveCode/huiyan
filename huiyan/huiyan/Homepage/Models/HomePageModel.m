13f95e265ef453e7cd610a9230c16874f5b516ad
maxLoveCode <maxkidy@gmail.com>
2016-04-06 15:09:00 +0800

    access token

--- huiyan/huiyan/Homepage/Models/HomePageModel.m
+++ huiyan/huiyan/Homepage/Models/HomePageModel.m
@@ -10,4 +10,18 @@
 
 @implementation HomePageModel
 
++(HomePageModel* )parseDramaJSON:(NSDictionary*)json
+{
+    HomePageModel* drama = [[HomePageModel alloc] init];
+    [drama setCover:[json objectForKey:@"cover"]];
+    [drama setContent:[json objectForKey:@"content"]];
+    [drama setCid:(NSInteger*)[[json objectForKey:@"cid"] integerValue]];
+    [drama setType:(NSInteger*)[[json objectForKey:@"type"] integerValue]];
+    [drama setID:(NSInteger*)[[json objectForKey:@"id"] integerValue]];
+    [drama setActor:[json objectForKey:@"actor"]];
+    [drama setTitle:[json objectForKey:@"title"]];
+    
+    return drama;
+}
+
 @end
