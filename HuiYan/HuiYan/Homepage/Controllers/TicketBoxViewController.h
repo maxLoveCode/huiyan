6d273f0e14813a9febe8a6157509376e429ab9b9
maxLoveCode <maxkidy@gmail.com>
2016-04-06 16:03:20 +0800

    ticketbox

---
+++ HuiYan/HuiYan/Homepage/Controllers/TicketBoxViewController.h
@@ -0,0 +1,13 @@
+//
+//  TicketBoxViewController.h
+//  huiyan
+//
+//  Created by 华印mac-001 on 16/4/6.
+//  Copyright © 2016年 com.huayin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TicketBoxViewController : UIViewController
+
+@end
--- huiyan/huiyan/Homepage/Controllers/WikiViewController.m
+++ huiyan/huiyan/Homepage/Controllers/WikiViewController.m
@@ -9,7 +9,10 @@
 #import "WikiViewController.h"
 #import "Constant.h"
 #import "HomePageCell.h"
+#import "MCSwipableMenuViewController.h"
+
 #define kLineNumber 3
+
 @interface WikiViewController ()<UITableViewDelegate,UITableViewDataSource,UIScrollViewDelegate>
 @property (nonatomic, strong) UITableView *dramaTableView;
 @property (nonatomic, strong) UIView* head_view;
@@ -26,31 +29,26 @@
     self.title  = @"戏曲百科";
     self.view.backgroundColor = [UIColor whiteColor];
     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc]initWithImage:[UIImage imageNamed:@""] style:UIBarButtonItemStylePlain target:self action:@selector(search:)];
-    [self head_view];
-    [self dramaTableView];
+    [self.view addSubview:self.head_view];
+    NSLog(@"%@",_head_view);
+    [self.view addSubview:self.dramaTableView];
 }
 
 - (UIView *)head_view{
-    if (_head_view == nil) {
-        self.head_view = [[UIView alloc]initWithFrame:CGRectMake(0, 0, kScreen_Width, 41)];
+    if (!_head_view) {
+        _head_view = [[UIView alloc]init];
 
-        for (int i = 0; i < 5; i++) {
-            NSArray *title = @[@"全部",@"京剧",@"昆剧",@"豫剧",@"黄梅⬇️"];
-            UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
-            btn.tag = 100 + i;
-            btn.titleLabel.font = kFONT12;
-            [btn addTarget: self action:@selector(refreshData:) forControlEvents:UIControlEventTouchUpInside];
-            [btn setTitle:title[i] forState:UIControlStateNormal];
-            [btn setTitleColor:COLOR_WithHex(0x666666) forState:UIControlStateNormal];
-            btn.titleLabel.textAlignment = NSTextAlignmentCenter;
-            btn.frame = CGRectMake(kScreen_Width / 5 * i, 0, kScreen_Width / 5, 41);
-            [self.head_view addSubview:btn];
-        }
-         [self.view addSubview:self.head_view];
-        
+        MCSwipableMenuViewController *menuController = [[MCSwipableMenuViewController alloc] init];
+        [self addChildViewController:menuController];
+        menuController.menuHeight = 41.0;
+        menuController.menuWidth = kScreen_Width;
+        [_head_view setBackgroundColor:[UIColor greenColor]];
+        [_head_view addSubview:menuController.view];
+        [_head_view setFrame:menuController.view.frame];
     }
     return _head_view;
 }
+
 - (UITableView *)dramaTableView{
     if (_dramaTableView == nil) {
         self.dramaTableView = [[UITableView alloc]initWithFrame:CGRectMake(0,CGRectGetMaxY(self.head_view.frame), kScreen_Width, kScreen_Height - 193)];
@@ -59,7 +57,6 @@
         [self.dramaTableView registerClass:[HomePageCell class] forCellReuseIdentifier:@"drama"];
         self.dramaTableView.rowHeight = [HomePageCell cellHeight];
         self.dramaTableView.separatorStyle = NO;
-        [self.view addSubview:self.dramaTableView];
     }
     return _dramaTableView;
 }
