d48acfaf6fdd90e51a395a42a263bffb30dff93d
maxLoveCode <maxkidy@gmail.com>
2016-04-13 13:36:56 +0800

    login

---
+++ huiyan/huiyan/commonFiles/model/User.m
@@ -0,0 +1,23 @@
+//
+//  User.m
+//  huiyan
+//
+//  Created by 华印mac-001 on 16/4/13.
+//  Copyright © 2016年 com.huayin. All rights reserved.
+//
+
+#import "User.h"
+
+@interface User()
+
+@property (nonatomic, assign) NSInteger* userID;
+@property (nonatomic, copy) NSString* nickName;
+@property (nonatomic, copy) NSString* mobile;
+@property (nonatomic, copy) NSString* password;
+
+@end
+
+@implementation User
+
+
+@end
