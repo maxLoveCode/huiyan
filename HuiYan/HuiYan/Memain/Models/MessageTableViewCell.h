dd4480dbc2f14402e22feeae7a7eaa82326937fa
yanfeifengwu <15139097394@163.com>
2016-05-20 17:06:44 +0800

    homepage

---
+++ HuiYan/HuiYan/Memain/Models/MessageTableViewCell.h
@@ -0,0 +1,16 @@
+//
+//  MessageTableViewCell.h
+//  huiyan
+//
+//  Created by zc on 16/5/20.
+//  Copyright © 2016年 com.huayin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MessageTableViewCell : UITableViewCell
+
+@property (nonatomic, strong) UILabel* title;
+@property (nonatomic, strong) UILabel* content;
+
+@end
