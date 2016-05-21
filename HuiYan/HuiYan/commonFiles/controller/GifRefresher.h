570a42fae04749ade1b33afe0e88aa265899c0bb
yanfeifengwu <15139097394@163.com>
2016-05-21 17:05:20 +0800

    interst

---
+++ HuiYan/HuiYan/commonFiles/controller/GifRefresher.h
@@ -0,0 +1,13 @@
+//
+//  GifRefresher.h
+//  huiyan
+//
+//  Created by zc on 16/5/21.
+//  Copyright © 2016年 com.huayin. All rights reserved.
+//
+
+#import <MJRefresh/MJRefresh.h>
+
+@interface GifRefresher : MJRefreshGifHeader
+
+@end
---
+++ HuiYan/HuiYan/commonFiles/controller/GifRefresher.m
@@ -0,0 +1,21 @@
+//
+//  GifRefresher.m
+//  huiyan
+//
+//  Created by zc on 16/5/21.
+//  Copyright © 2016年 com.huayin. All rights reserved.
+//
+
+#import "GifRefresher.h"
+
+@implementation GifRefresher
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
